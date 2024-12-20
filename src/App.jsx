708b4ad2f63028cf24d065ff8ab9e6b99e4300b7
justwooki <79425101+justwooki@users.noreply.github.com>
2024-12-19 23:27:06 -0500

    Meet the React Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const title = 'React'
+  const welcome = {
+    greeting: 'Hey',
+    title: 'React',
+  }
 
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>
+        {welcome.greeting} {welcome.title}
+      </h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
     </div>
   )
 }
