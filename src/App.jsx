3d858354be27785f4bb409b5594f51db3b184bf6
justwooki <79425101+justwooki@users.noreply.github.com>
2024-12-27 18:50:58 -0500

    Lists in React

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,49 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const welcome = {
-    greeting: 'Hey',
+const list = [
+  {
     title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramox, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
   }
+]
 
+function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor='search'>Search: </label>
       <input id='search' type='text' />
+
+      <hr />
+
+      <ul>
+        {list.map(function(item) {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
