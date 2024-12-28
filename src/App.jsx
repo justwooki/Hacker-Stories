31f4d5b94631167e49f46308b35bbd331154425c
justwooki <79425101+justwooki@users.noreply.github.com>
2024-12-28 16:06:18 -0500

    React Controlled Components

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
       objectID: 1,
     }
   ]
-  const [searchTerm, setSearchTerm] = React.useState('')
+  const [searchTerm, setSearchTerm] = React.useState('React')
   const handleSearch = (event) => {
     setSearchTerm(event.target.value)
   }
@@ -32,7 +32,7 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      {<Search onSearch={handleSearch} />}
+      {<Search search={searchTerm} onSearch={handleSearch} />}
 
       <hr />
 
@@ -45,7 +45,12 @@ const Search = (props) => {
   return (
     <div>
       <label htmlFor='search'>Search: </label>
-      <input id='search' type='text' onChange={props.onSearch} />
+      <input
+        id='search'
+        type='text'
+        value={props.search}
+        onChange={props.onSearch}
+      />
     </div>
   )
 }
