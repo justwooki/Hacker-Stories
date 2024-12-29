8eb129a655549ac349b579e13085ff38e1828b28
justwooki <79425101+justwooki@users.noreply.github.com>
2024-12-29 02:15:18 -0500

    Explicit Data Fetching with React

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,8 @@ const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
 
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
     data: [],
     isLoading: false,
@@ -58,11 +60,11 @@ const App = () => {
   });
 
   const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return
+    if (!searchTerm) return;
 
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -71,10 +73,10 @@ const App = () => {
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
-  }, [searchTerm])
+  }, [url]);
 
   React.useEffect(() => {
-    handleFetchStories()
+    handleFetchStories();
   }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
@@ -84,8 +86,12 @@ const App = () => {
     });
   };
 
-  const handleSearch = (event) => {
+  const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
   return (
@@ -96,10 +102,14 @@ const App = () => {
         id="search"
         value={searchTerm}
         isFocused
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
       >
         <strong>Search:</strong>
       </InputWithLabel>
+
+      <button type="button" disabled={!searchTerm} onClick={handleSearchSubmit}>
+        Submit
+      </button>
 
       <hr />
 
