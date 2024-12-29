3270f67c234f73b2d7a5fbaa0ce3fd86918f7d66
justwooki <79425101+justwooki@users.noreply.github.com>
2024-12-28 23:22:23 -0500

    React Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -47,11 +47,17 @@ const App = () => {
   )
 
   const [stories, setStories] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(false)
+  const [isError, setIsError] = React.useState(false)
 
   React.useEffect(() => {
+    setIsLoading(true)
+
     getAsyncStories().then(result => {
       setStories(result.data.stories)
+      setIsLoading(false)
     })
+    .catch(() => setIsError(true))
   }, [])
 
   const handleRemoveStory = (item) => {
@@ -85,7 +91,13 @@ const App = () => {
 
       <hr />
 
-      {<List list={searchedStories} onRemoveItem={handleRemoveStory} />}
+      {isError && <p>Something went wrong ...</p>}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </>
   )
 }
